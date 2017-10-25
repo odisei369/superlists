eaa861139ba162d99391da9b5ef38f98affaa24a
odisei369 <petrenko.ilia@gmail.com>
2017-10-25 10:02:42 +0200

    complete unit tests

--- lists/tests.py
+++ lists/tests.py
@@ -1,6 +1,7 @@
 from django.test import TestCase
 from django.urls import resolve
 from lists.views import home_page
+from django.http import HttpRequest
 
 class HomePageTest(TestCase):
 
@@ -8,7 +9,10 @@ class HomePageTest(TestCase):
 		found = resolve('/')
 		self.assertEqual(found.func, home_page)
 
-class SmokeTest(TestCase):
-
-	def test_bad_math(self):
-		self.assertEqual(1+1, 3)
+	def test_home_page_returns_correct_html(self):
+		request = HttpRequest()
+		response = home_page(request)
+		html = response.content.decode('utf8')
+		self.assertTrue(html.startswith('<html>'))
+		self.assertIn('<title>To-Do lists</title>', html)
+		self.assertTrue(html.endswith('</html>'))
