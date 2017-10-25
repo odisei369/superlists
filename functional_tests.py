91b3984424bd3f396071d1ee77812b3ffa962360
odisei369 <petrenko.ilia@gmail.com>
2017-10-25 23:10:56 +0200

    integration tests passed

--- functional_tests.py
+++ functional_tests.py
@@ -3,23 +3,6 @@ import unittest
 import time
 from selenium.webdriver.common.keys import Keys
 
-class ItemModelTest(TestCase):
-	def test_saving_and_retrieving_items(self):
-		first_item = Item()
-		first_item.text = 'The first (ever) list item'
-		first_item.save()
-
-		second_item = Item()
-		second_item.text = 'Item the second'
-		second_item.save()
-
-		saved_items = Item.objects.all()
-		self.assertEqual(saved_items.count(), 2)
-
-		first_saved_item = saved_items[0]
-		second_saved_item = saved_items[1]
-		self.assertEqual(first_saved_item.text, 'The first (ever) list item')
-		self.assertEqual(second_saved_item.text, 'Item the second')
 
 
 class NewVisitorTest(unittest.TestCase):
