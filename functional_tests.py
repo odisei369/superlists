517f9b9cf59fa6dcee22d51044a3bf6576044cbc
odisei369 <petrenko.ilia@gmail.com>
2017-10-25 11:15:31 +0200

    implement simple html

--- functional_tests.py
+++ functional_tests.py
@@ -1,5 +1,7 @@
 from selenium import webdriver
 import unittest
+import time
+from selenium.webdriver.common.keys import Keys
 
 class NewVisitorTest(unittest.TestCase):
 	def setUp(self):
@@ -13,7 +15,7 @@ class NewVisitorTest(unittest.TestCase):
 		self.assertIn('To-Do', self.browser.title)
 		
 		header_text = self.browser.find_element_by_tag_name('h1').text
-		selg.assertIn('To-Do', header_text)
+		self.assertIn('To-Do', header_text)
 
 		inputbox = self.browser.find_element_by_id('id_new_item')
 		self.assertEqual(
@@ -29,7 +31,8 @@ class NewVisitorTest(unittest.TestCase):
 		table = self.browser.find_element_by_id('id_list_table')
 		rows = table.find_elements_by_tag_name('tr')
 		self.assertTrue(
-			any(row.text == '1: Buy peacock feathers' for row in rows)
+			any(row.text == '1: Buy peacock feathers' for row in rows),
+			"New to-do item did not appear in table"
 		)
 		
 		self.fail('Finish the test!')
