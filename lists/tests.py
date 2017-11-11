16660fe1d111123bd4b9c0d5d78ef1282ea3c9d3
Piatrenka Ilya <petrenko.ilia@gmail.com>
2017-11-11 15:41:02 +0100

    items are associated with lists

--- lists/tests.py
+++ lists/tests.py
@@ -2,7 +2,7 @@ from django.test import TestCase
 from django.urls import resolve
 from lists.views import home_page
 from django.http import HttpRequest
-from lists.models import Item
+from lists.models import Item, List
 
 
 class NewListTest(TestCase):
@@ -25,23 +25,33 @@ class ListViewTest(TestCase):
 		self.assertTemplateUsed(response, 'list.html')
 
 	def test_displays_all_items(self):
-		Item.objects.create(text='item 1')
-		Item.objects.create(text='item 2')
+		list_ = List.objects.create()
+		Item.objects.create(text='item 1', list=list_)
+		Item.objects.create(text='item 2', list=list_)
 		response = self.client.get('/lists/the_only_list/')
 
 		self.assertContains(response, 'item 1')
 		self.assertContains(response, 'item 2')
 
 
-class ItemModelTest(TestCase):
+class ListAndItemModelTest(TestCase):
 	def test_saving_and_retrieving_items(self):
+		list_ = List()
+		list_.save()
+
 		first_item = Item()
 		first_item.text = 'The first (ever) list item'
+		first_item.list = list_
 		first_item.save()
 
 		second_item = Item()
 		second_item.text = 'Item the second'
+		second_item.list = list_
 		second_item.save()
+
+		saved_list = List.objects.first()
+		self.assertEqual(saved_list, list_)
+
 
 		saved_items = Item.objects.all()
 		self.assertEqual(saved_items.count(), 2)
@@ -49,7 +59,9 @@ class ItemModelTest(TestCase):
 		first_saved_item = saved_items[0]
 		second_saved_item = saved_items[1]
 		self.assertEqual(first_saved_item.text, 'The first (ever) list item')
+		self.assertEqual(first_saved_item.list, list_)
 		self.assertEqual(second_saved_item.text, 'Item the second')
+		self.assertEqual(second_saved_item.list, list_)
 
 
 class HomePageTest(TestCase):
@@ -63,15 +75,15 @@ class HomePageTest(TestCase):
 		self.assertTemplateUsed(response, 'home.html')
 
 	def test_can_save_a_POST_request(self):
-		response = self.client.post('/', data={'item_text' : 'A new list item'})
+		response = self.client.post('/lists/new', data={'item_text' : 'A new list item'})
 
 		self.assertEqual(Item.objects.count(), 1)
 		new_item = Item.objects.first()
 		self.assertEqual(new_item.text, 'A new list item')
 
 	def test_redirect_after_POST(self):
-		response = self.client.post('/', data={'item_text' : 'A new list item'})
+		response = self.client.post('/lists/new', data={'item_text' : 'A new list item'})
 
 		self.assertEqual(response.status_code, 302)
-		self.assertEqual(response['location'], '/lists/the_only_list')
+		self.assertEqual(response['location'], '/lists/the_only_list/')
 
